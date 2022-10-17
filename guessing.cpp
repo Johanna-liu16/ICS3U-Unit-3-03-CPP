5894bc83c571ffc2bde0b1345bddeff7a1c81ac6
Johanna Liu <johanna.liu@mths.ca>
2022-10-17 15:07:36 +0000

    init

--- guessing.cpp
+++ guessing.cpp
@@ -4,13 +4,18 @@
 // Created on: Sep 2022
 // This program is a random number guessing game
 
-#include <iomanip>
 #include <iostream>
+#include <random>
 
 int main() {
     // this function generates a random number
-    int randomNumber;
     int userNumber;
+    int randomNumber;
+
+    std::random_device rseed;
+    std::mt19937 rgen(rseed());  // mersenne_twister
+    std::uniform_int_distribution<int> idist(0, 9);  // [0,9]
+    randomNumber = idist(rgen);
 
     // input
     std::cout << "This program is a guessing game." << std::endl;
@@ -18,14 +23,12 @@ int main() {
     std::cin >> userNumber;
 
     // process
-    if (userNumber == SPECIAL_NUMBER) {
+    if (userNumber == randomNumber) {
         // output
         std::cout << "YAY! You guessed the correct number!";
-    }
-
-    if (userNumber != SPECIAL_NUMBER) {
+    } else {
         // output
-        std::cout << "Try again";
+        std::cout << "Random number was " << randomNumber << ". Try again.";
     }
 
     std::cout << std::endl;
